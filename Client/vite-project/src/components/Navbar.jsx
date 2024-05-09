d5426c0ac8ad0f2e137e2e4bcfd0f9b7288085c9
Jagriti Sharma <sharmajr@rknec.edu>
2024-05-09 23:07:27 +0530

    Frontend done

--- Client/vite-project/src/components/Navbar.jsx
+++ Client/vite-project/src/components/Navbar.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
+import React,{ useContext} from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../context/App_Context";
 
 const Navbar = () => {
+    const {isAuthenticated, setisAuthenticated,logOut} = useContext(AppContext)
   
   return (
     <>
-      <div className="nav bg-dark p-2">
-       <div className="left"><h2>MERN recipies</h2></div>
-       <div className="right">
-        <div className="btn btn-primary mx-3">Login</div>
-        <div className="btn btn-warning mx-3">Register</div>
-        <div className="btn btn-info mx-3">Add</div>
-        <div className="btn btn-warning mx-3">Profile</div>
-        <div className="btn btn-danger mx-3">Logout</div>
-        <div className="btn btn-light mx-3">Saved</div>
-       </div>
+    <div className="nav bg-dark p-2">
+      <Link
+        to={"/"}
+        className="left"
+        style={{ textDecoration: "none", color: "white" }}
+      >
+        <h2>MERN Recipe</h2>
+      </Link>
+      <div className="right">
+        {isAuthenticated && (
+          <>
+            <Link to={"/add"} className="btn btn-info mx-2">
+              Add
+            </Link>
+            <Link to={"/profile"} className="btn btn-warning mx-2">
+              Profile
+            </Link>
+            <div className="btn btn-danger mx-2" onClick={logOut}>LogOut</div>
+          </>
+        )}
+        {!isAuthenticated && (
+          <>
+            <Link to={"/login"} className="btn btn-primary mx-2">
+              Login
+            </Link>
+            <Link to={"/register"} className="btn btn-warning mx-2">
+              Register
+            </Link>
+          </>
+        )}
+
+        <Link to={"/saved"} className="btn btn-light mx-2">
+          Saved
+        </Link>
+      </div>
     </div>
     </>
-  )
-}
-        
-          
+  );
+};
+                
 
 export default Navbar;
