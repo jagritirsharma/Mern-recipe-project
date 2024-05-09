6c72e7633761c15cb59db9356623f39a51b03a27
Jagriti Sharma <sharmajr@rknec.edu>
2024-05-09 10:31:10 +0530

    Backend code done

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,8 +1,13 @@
 import express from 'express'
-import {register} from '..controllers/user.js';
+import {register, login, profile} from '../controllers/user.js';
+import { Authenticate } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post('/login',login)
+
+router.get('/user',Authenticate,profile)
 
 export default router
